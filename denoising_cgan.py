51017d632876f54ef7e387754d7da9e0f1c061cc
ncortese <32407830+ncortese@users.noreply.github.com>
2017-10-10 14:05:56 -0500

    Update denoising_cgan.py

--- denoising_cgan.py
+++ denoising_cgan.py
@@ -1,39 +1,38 @@
 import tensorflow as tf
 import nn_construction as nnc
 import numpy as np
-
-# debug
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+from batch_loader import BatchLoader
+from PIL import Image
+import os
 
 # parameters
 batch_size = 20
-num_steps = 10000
-image_height = 28
-image_width = 28
-image_channels = 1
+num_steps = 5000
+image_height = 8
+image_width = 8
+image_channels = 3
 image_size = image_height*image_width
-activation = tf.nn.elu
+activation = tf.nn.relu
 
 # discriminator network architecture
-disc_conv_layers = ['c']
-disc_filter_shapes = [[4, 4]]
-disc_strides = [[1, 1]]
-disc_filter_nums = [24]
-disc_dense_layers = ['f']
-disc_dense_sizes = [1]
-disc_dropout_rates = [None]
+disc_conv_layers = ['c', 'c', 'c', 'c']
+disc_filter_shapes = [[4, 4], [4, 4], [4, 4], [1, 1]]
+disc_strides = [[1, 1], [2, 2], [2, 2], [2, 2]]
+disc_filter_nums = [8, 16, 16, 1]
+disc_dense_layers = []
+disc_dense_sizes = []
+disc_dropout_rates = []
 
 # generator network architecture
-gen_conv_layers = []
-gen_filter_shapes = []
-gen_strides = []
-gen_filter_nums = []
-gen_dense_layers = ['f', 'f']
-gen_dense_sizes = [128, 784]
-gen_dropout_rates = [None, None]
+gen_conv_layers = ['t', 't', 't']
+gen_filter_shapes = [[4, 4], [4, 4], [1, 1]]
+gen_strides = [[1, 1], [2, 2], [1, 1]]
+gen_filter_nums = [32, 16, 3]
+gen_dense_layers = []
+gen_dense_sizes = []
+gen_dropout_rates = []
 latent_size = 32  # dimensionality of the noise for the generator
-noise_type = tf.random_normal  # distribution of the noise for the generator
+noise_type = tf.random_uniform  # distribution of the noise for the generator
 
 
 # build discriminator, following convolutional layers with dense layers
@@ -61,10 +60,11 @@ def generator(in_layer, reuse):
         out_layer = tf.reshape(out_layer, [batch_size, 4, 4, -1])
         out_layer = nnc.build_conv_network(out_layer, gen_conv_layers, gen_filter_shapes, gen_strides,
                                            gen_filter_nums, activation, reuse)
+    out_layer = tf.reshape(out_layer, [batch_size, -1])
     return out_layer
 
-training_input = tf.placeholder(tf.float32, [batch_size, image_size])
-cond_input = tf.placeholder(tf.float32, [batch_size, image_size])
+training_input = tf.placeholder(tf.float32, [batch_size, image_size*image_channels])
+cond_input = tf.placeholder(tf.float32, [batch_size, image_size*image_channels])
 gen_noise = noise_type([batch_size, latent_size])
 
 # get outputs of generator
@@ -90,46 +90,56 @@ disc_gen_accuracy = tf.reduce_mean(disc_gen_output)
 disc_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='disc')
 gen_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='gen')
 
-# disc_minimize = tf.train.AdamOptimizer().minimize(disc_loss, var_list=disc_variables)
-# gen_minimize = tf.train.AdamOptimizer().minimize(gen_loss, var_list=gen_variables)
+disc_minimize = tf.train.AdamOptimizer(learning_rate=2E-4, beta1=0.5).minimize(disc_loss, var_list=disc_variables)
+gen_minimize = tf.train.AdamOptimizer(learning_rate=2E-4, beta1=0.5).minimize(gen_loss, var_list=gen_variables)
 
 # clipped optimization for discriminator
-with tf.name_scope("Adam_optimizer_disc"):
-    optimizer = tf.train.AdamOptimizer(1E-4)
-    grads_and_vars = optimizer.compute_gradients(disc_loss, var_list=disc_variables)
-    clipped = [(tf.clip_by_value(grad, -5, 5), tvar)  # gradient clipping
-               for grad, tvar in grads_and_vars]
-    train_step_disc = optimizer.apply_gradients(clipped, name="minimize_cost")
+# with tf.name_scope("Adam_optimizer_disc"):
+#     optimizer = tf.train.AdamOptimizer(1E-4)
+#     grads_and_vars = optimizer.compute_gradients(disc_loss, var_list=disc_variables)
+#     clipped = [(tf.clip_by_value(grad, -5, 5), tvar)  # gradient clipping
+#                for grad, tvar in grads_and_vars]
+#     train_step_disc = optimizer.apply_gradients(clipped, name="minimize_cost")
+#
+# # clipped optimization for generator
+# with tf.name_scope("Adam_optimizer_gen"):
+#     optimizer = tf.train.AdamOptimizer(1E-4)
+#     grads_and_vars = optimizer.compute_gradients(gen_loss, var_list=gen_variables)
+#     clipped = [(tf.clip_by_value(grad, -5, 5), tvar)  # gradient clipping
+#                for grad, tvar in grads_and_vars]
+#     train_step_gen = optimizer.apply_gradients(clipped, name="minimize_cost")
 
-# clipped optimization for generator
-with tf.name_scope("Adam_optimizer_gen"):
-    optimizer = tf.train.AdamOptimizer(1E-4)
-    grads_and_vars = optimizer.compute_gradients(gen_loss, var_list=gen_variables)
-    clipped = [(tf.clip_by_value(grad, -5, 5), tvar)  # gradient clipping
-               for grad, tvar in grads_and_vars]
-    train_step_gen = optimizer.apply_gradients(clipped, name="minimize_cost")
+batches = BatchLoader('./berkeley_patches', './berkeley_noise')
 
-# Running on MNIST at first, for testing
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(num_steps):
-        batch, _ = mnist.train.next_batch(batch_size)
+        batch_patches, batch_noise = batches.get_next_batch(batch_size)
 
-        # Sloppy temporary way of adding (masking) noise to each MNIST sample consistently (will remove this after building
-        # actual dataset of 8x8 patches)
-        noisy_batch = np.zeros([batch_size, image_size])
-        for j in range(batch_size):
-            seed_j = np.sum(batch[j, :]).astype(np.int64)
-            np.random.seed(seed_j)
-            noise = np.random.randint(2, size=image_size)
-            noisy_batch[j, :] = np.multiply(batch[j, :], noise)
+        batch_patches = np.array(batch_patches)
+        batch_noise = np.array(batch_noise)
+        batch_patches = np.reshape(batch_patches, (batch_size, -1))
+        batch_noise = np.reshape(batch_noise, (batch_size, -1))
+        batch_patches = batch_patches.astype(np.float32)
+        batch_noise = batch_noise.astype(np.float32)
+        batch_patches = batch_patches/255.
+        batch_noise = batch_noise/255.
 
-        # disc_minimize.run(feed_dict={training_input: batch, cond_input: noisy_batch})
-        # gen_minimize.run(feed_dict={cond_input: noisy_batch})
-        sess.run(train_step_disc, feed_dict={training_input: batch, cond_input: noisy_batch})
-        sess.run(train_step_gen, feed_dict={cond_input: noisy_batch})
+        sess.run(disc_minimize, feed_dict={training_input: batch_patches, cond_input: batch_noise})
+        sess.run(gen_minimize, feed_dict={cond_input: batch_noise})
         if i % 100 == 0:
-            current_disc_accuracy = disc_train_accuracy.eval(feed_dict={training_input: batch, cond_input: noisy_batch})
-            current_gen_accuracy = disc_gen_accuracy.eval(feed_dict={training_input: batch, cond_input: noisy_batch})
+            current_disc_accuracy = disc_train_accuracy.eval(feed_dict={training_input: batch_patches,
+                                                                        cond_input: batch_noise})
+            current_gen_accuracy = disc_gen_accuracy.eval(feed_dict={training_input: batch_patches,
+                                                                     cond_input: batch_noise})
             print('step {}, disc. train accuracy {:f}, disc. gen. accuracy {:f}'.format(i, current_disc_accuracy,
                                                                                         current_gen_accuracy))
+            # save a generated sample
+            samples = gen_output.eval(feed_dict = {cond_input: batch_noise})
+            samples = np.reshape(samples, (batch_size, image_height, image_width, image_channels))
+            noise_1 = np.reshape(batch_noise[0, :], (image_height, image_width, image_channels))
+            data_1 = np.reshape(batch_patches[0, :], (image_height, image_width, image_channels))
+            sample_1 = samples[0, :, :, :]
+            image = np.concatenate((data_1, noise_1, sample_1), axis=1)
+            image = Image.fromarray(np.uint8(image*255))
+            image.save(os.path.join('./generated_samples', "iteration_" + str(i) + ".jpg"), "JPEG")
