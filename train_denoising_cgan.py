874955ca03d4d7edc0ec49a75082414c49ee137d
ncortese <32407830+ncortese@users.noreply.github.com>
2017-12-12 21:22:40 -0600

    Update train_denoising_cgan.py
    Modified slightly to work with changes to CGAN

--- train_denoising_cgan.py
+++ train_denoising_cgan.py
@@ -1,5 +1,5 @@
 from resid_conv_cgan import CGAN
-from batch_loader import BatchLoader
+from infinite_denoising_batch_loader import BatchLoader
 import tensorflow as tf
 
 
@@ -8,32 +8,31 @@ image_height = 8
 image_width = 8
 image_channels = 3
 
-# noise_dev = 255./8.
+noise_dev = 255./8.
 
 batch_size = 64
-num_steps = 10000
+num_steps = 8000
 
-latent_size = 32  # dimensionality of the noise for the generator
+# latent_size = 32  # dimensionality of the noise for the generator
 noise_type = tf.random_uniform  # distribution of the noise for the generator
 
 leaky_relu_alpha = 0.2
 
 disc_depth = 3
-disc_filter_sizes = [(4, 4), (4, 4), (4, 4)]
-disc_filter_nums = [32, 24, 16]
+disc_filter_sizes = [(4, 4), (3, 3), (3, 3)]
+disc_filter_nums = [64, 128, 128]
 disc_strides = [(1, 1), (2, 2), (1, 1)]
 
-gen_init_shape = (4, 4, 32)
-gen_depth = 3
-gen_filter_sizes = [(4, 4), (4, 4), (4, 4)]
-gen_filter_nums = [32, 24, 16]
-gen_strides = [(1, 1), (2, 2), (1, 1)]
+gen_depth = 4
+gen_filter_sizes = [(4, 4), (4, 4), (4, 4), (4, 4)]
+gen_filter_nums = [64, 64, 64, 64]
+gen_strides = [(1, 1), (1, 1), (1, 1), (1, 1)]
 
-cgan = CGAN(image_height, image_width, image_channels, latent_size, noise_type, leaky_relu_alpha, disc_depth,
-            disc_filter_nums, disc_filter_sizes, disc_strides, gen_init_shape, gen_depth, gen_filter_nums,
+cgan = CGAN(image_height, image_width, image_channels, noise_type, leaky_relu_alpha, disc_depth,
+            disc_filter_nums, disc_filter_sizes, disc_strides, gen_depth, gen_filter_nums,
             gen_filter_sizes, gen_strides)
 
-# batches = BatchLoader('./berkeley data/train/', (image_height, image_width), image_channels, noise_dev)
-batches = BatchLoader('./berkeley_patches/', './berkeley_noise/')
+batches = BatchLoader('./berkeley data/train/', (image_height, image_width), image_channels, noise_dev)
 
-cgan.train(batches, batch_size, num_steps, './saved_models/', './generated_samples/')
+# Load from a saved model by cgan.train(batches, batch_size, num_steps, './saved_models/', './generated_samples/', True, n), where n is iteration number of the saved model
+cgan.train(batches, batch_size, num_steps, './saved_models/', './generated_samples/', False)
